61adf61f0ec55f393297cdc1fcd932911e34ff64
adamZdrojewski <adam.zdrojewski2@gmail.com>
2021-04-29 10:37:18 -0400

    fixed navbar scroll offset issue, also added favicon

--- scrollDetect.js
+++ scrollDetect.js
@@ -1,5 +1,16 @@
+//Constants
 const nav = document.querySelector("nav");
+
+//Page Setup
+testScrollPosition();
+
+//Event Listeners
 window.addEventListener("scroll", (e) => {
+    testScrollPosition();
+});
+
+//Functions
+function testScrollPosition() {
     if(window.scrollY <= 0) {
         //Page Scrolled All The Way Up
         nav.style.backgroundColor = "";
@@ -7,4 +18,20 @@ window.addEventListener("scroll", (e) => {
         //Page Not Scrolled At The Top
         nav.style.backgroundColor = "#008FDB";
     }
-});
+}
+
+function myScroll(section) {
+    if(section == "home") {
+        document.querySelector("#home").scrollIntoView(true);
+    } else if(section == "aboutMe") {
+        document.querySelector("#aboutMe").scrollIntoView(true);
+    } else if(section == "projects") {
+        document.querySelector("#projects").scrollIntoView(true);
+    } else if(section == "social") {
+        document.querySelector("#social").scrollIntoView(true);
+    }
+    var maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    if(window.scrollY && maxScroll != window.scrollY) {
+        window.scroll(0, scrollY - 89);
+    }
+}
